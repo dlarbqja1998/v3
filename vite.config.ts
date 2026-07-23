c2ad313d7457ff7b580f44cbf85d40d27b37cac8
dlarbqja1998 <dlarbqja19980987@gmail.com>
2026-07-23 13:37:25 +0900

    개발 환경 및 폰트 설정 반영

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	server: {
+		allowedHosts: [
+			'softball-shows-selecting-doors.trycloudflare.com',
+			'dating-head-routes-beer.trycloudflare.com'
+		]
+	},
 	plugins: [
 		tailwindcss(),
 		sveltekit({
